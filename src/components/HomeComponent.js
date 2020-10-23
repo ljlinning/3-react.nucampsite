3ed24c2584348754752a5d194bad4f21f39da0f7
Logan Linning <ljlinning@gmail.com>
2020-10-22 22:41:29 -0500

    fix HomeComponent.js file

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -10,23 +10,26 @@ function RenderCard({item}) {
                 <CardText>{item.description}</CardText>
             </CardBody>
         </Card>
-
-function Home(props) {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md m-1">
-                    <RenderCard item={props.campsite} />
-                </div>
-                <div className="col-md m-1">
-                    <RenderCard item={props.promotion} />
-                </div>
-                <div className="col-md m-1">
-                    <RenderCard item={props.partner} />
-                </div>
-            </div>
-        </div>
     );
 }
+
+
+    function Home(props) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md m-1">
+                        <RenderCard item={props.campsite} />
+                    </div>
+                    <div className="col-md m-1">
+                        <RenderCard item={props.promotion} />
+                    </div>
+                    <div className="col-md m-1">
+                        <RenderCard item={props.partner} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
 export default Home;   
