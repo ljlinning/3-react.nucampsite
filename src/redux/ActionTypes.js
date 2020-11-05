fdfb595101ac7387626d4475060bcdee76c7fa48
Logan Linning <ljlinning@gmail.com>
2020-11-04 23:16:52 -0600

    Redux Thunk

--- src/redux/ActionTypes.js
+++ src/redux/ActionTypes.js
@@ -1,1 +1,28 @@
+import { actionTypes } from 'react-redux-form';
+import { CAMPSITES } from '../shared/campsites';
+
+
 export const ADD_COMMENT = 'ADD_COMMENT';
+
+export const fetchCampsites = () => dispatch => {
+
+    dispatch(campsitesLoading());
+
+    setTimeout(() => {
+        dispatch(addCampsites(CAMPSITES));
+    }, 2000);
+};
+
+export const campsitesLoading = () => ({
+    type: ActionTypes.CAMPSITES_LOADING
+});
+
+export const campsitesFailed = errMess => ({
+    type: ActionTypes.CAMPSITES_FAILED,
+    payload: errMess
+});
+
+export const addCampsites = campsites => ({
+    type: ActionTypes.ADD_CAMPSITES,
+    payload: campsites
+});
